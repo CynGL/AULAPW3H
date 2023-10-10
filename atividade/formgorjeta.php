bd1690f1b29e6915431506cf6dcc35bf261c0716
Cyntia Lino <100882616+CynGL@users.noreply.github.com>
2023-10-09 22:02:59 -0300

    Add files via upload

---
+++ atividade/formgorjeta.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Calculadora de Gorjeta</title>
+</head>
+<body>
+	 <form action="#" method="post">
+
+		  	
+				   <label for="cod">Valor da Conta:</label>
+				  <input type="number" name="valorc">
+				
+				
+
+				  <p>Qualidade de serviço:</p>
+				  <input type="radio" name="qs-a">
+				  <label for="excelente">Excelente - 10%</label>
+				  <input type="radio" name="qs-b">
+				  <label for="excelente">Ótimo - 8%</label>
+				  <input type="radio" name="qs-c">
+				  <label for="excelente">Bom - 5%</label>
+				  <input type="radio" name="qs-d">
+				  <label for="excelente">Ruim - 2%</label><br><br>		
+    			<button type="submit">Calcular Gorjeta</button><br><br>
+  			
+    			<label for="valorgt">Valor da gorjeta:</label>
+    			<input type="number" name="valorgt"><br><br>
+
+    			<label for="#">Valor Total:</label>
+    			<input type="number" name="valortt">
+		  </form>
+
+
+</body>
+</html>
