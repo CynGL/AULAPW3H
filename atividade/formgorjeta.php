88949ece023ef8a29039b817b53614ea7ac59abc
Cyntia Lino <100882616+CynGL@users.noreply.github.com>
2023-10-11 22:08:51 -0300

    Add files via upload

--- atividade/formgorjeta.php
+++ atividade/formgorjeta.php
@@ -3,35 +3,76 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Calculadora de Gorjeta</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<title>Formulário</title>
 </head>
+<style type="text/css">
+	
+body{
+	margin: 0;
+	width: 100vw;
+	height: 100vh;
+	background-image: linear-gradient(45deg, #060126, #030085);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-family: OCR A Std, monospace;
+	font-weight: 500;
+}
+
+form{
+	width: 60vw;
+	height: 70vh;
+	background-color: white;
+	border-radius: 30px;
+	padding: 20px;
+	justify-content: center;
+	align-items: center;
+	box-shadow: 0 8px 35px 0 rgba( 31, 38, 135, 0.40 );
+}
+
+
+
+
+
+
+
+
+</style>
 <body>
-	 <form action="#" method="post">
+	<div class="formulario">
+		  <form action="testa_funcionario.php" method="post">
 
-		  	
-				   <label for="cod">Valor da Conta:</label>
-				  <input type="number" name="valorc">
+		  	<div>
+		  		    <label for="cod">Valor da conta:</label>
+				  <input type="number" class="form-control" id="cod" placeholder="Insira o valor da conta" name="valor"><br>
 				
-				
+				</div>
 
-				  <p>Qualidade de serviço:</p>
-				  <input type="radio" name="qs-a">
+		  	<div>
+		  		<p>Qualidade de serviço:</p>
+				  <input type="radio" name="porcex">
 				  <label for="excelente">Excelente - 10%</label>
-				  <input type="radio" name="qs-b">
+				  <input type="radio" name="porcot">
 				  <label for="excelente">Ótimo - 8%</label>
-				  <input type="radio" name="qs-c">
+				  <input type="radio" name="porcbo">
 				  <label for="excelente">Bom - 5%</label>
-				  <input type="radio" name="qs-d">
-				  <label for="excelente">Ruim - 2%</label><br><br>		
-    			<button type="submit">Calcular Gorjeta</button><br><br>
-  			
-    			<label for="valorgt">Valor da gorjeta:</label>
-    			<input type="number" name="valorgt"><br><br>
+				  <input type="radio" name="porcru">
+				  <label for="excelente">Ruim - 2%</label><br><br>
+				  
+				</div>
 
-    			<label for="#">Valor Total:</label>
-    			<input type="number" name="valortt">
+				<div class="col-12">
+    			<button class="btn btn-primary" type="submit" name="operacao">Calcular Gorjeta</button><br><br>
+  			</div>
+		  	
+		  	
+		  	
+
+
+		  
+
 		  </form>
-
-
+  </div>
 </body>
 </html>
---
+++ atividade/gorjeta.php
@@ -0,0 +1,32 @@
+<?php 
+class res {
+
+	protected $valor;
+	public $operacao
+	public $porcex;
+	public $porcot;
+	public $porcbo;
+	public $porcru;
+	
+
+
+
+	function calcular ($operacao){
+
+		$a = $_POST['porcex'];
+		$b = $_POST['porcot'];
+		$c = $_POST['porcbo'];
+		$d = $_POST['porcru'];
+		$op = $_POST['operacao'];
+
+		if ($op  == ) {
+			// code...
+		}
+	}
+}
+
+
+
+
+
+ ?>
